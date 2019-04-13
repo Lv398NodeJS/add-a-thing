5bac131041a592447eafbcecd8500905cfdcce08
Mykhailo <mishalisovskiy@gmail.com>
2019-04-13 15:04:12 +0300

    Firebase and mainview

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Dashboard from './Dashboard';
+import App from './App';
 
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
