2d35487a0cf37ad10999c755316c6af52b90a5ab
Mykhailo <mishalisovskiy@gmail.com>
2019-04-13 14:43:17 +0300

    Resolving conflicts v.2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Dashboard from './Dashboard';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
