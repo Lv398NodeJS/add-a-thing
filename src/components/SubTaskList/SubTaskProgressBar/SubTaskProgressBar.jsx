a4375d4b4fc8891431bf013000feda6a673c7b1d
Vitaliy Tkachuk <vitfl@protonmail.com>
2019-04-30 17:37:10 +0300

    Minor refactoring; Move progressBarVariant function from SubTaskProgressBar component into separate file.

--- src/components/SubTaskList/SubTaskProgressBar/SubTaskProgressBar.jsx
+++ src/components/SubTaskList/SubTaskProgressBar/SubTaskProgressBar.jsx
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
 import { Row, Col, ProgressBar } from 'react-bootstrap';
+import getProgressBarVariant from './getProgressBarVariant';
 
 class SubTaskAdd extends Component {
   render() {
-    const { subtaskList } = this.props;
+    const subtaskList = this.props.subtaskList || [];
     const completedSubTasks = subtaskList.filter(subTask => subTask.completed);
     const completedSubTasksPercent = (completedSubTasks.length / subtaskList.length) * 100;
-    const progressBarVariant = (percent) => {
-      switch (true) {
-        case percent === 100:
-          return 'success';
-        case percent > 50:
-          return 'info';
-        case percent > 30:
-          return 'warning';
-        default:
-          return 'secondary';
-      }
-    };
     return (
       <Row className="justify-content-sm-center">
         <Col>
           <ProgressBar
-            className="mb-3"
+            className="subtask-progressbar mb-3"
             animated
             now={completedSubTasksPercent}
-            variant={progressBarVariant(completedSubTasksPercent)}
+            variant={getProgressBarVariant(completedSubTasksPercent)}
           />
         </Col>
       </Row>
