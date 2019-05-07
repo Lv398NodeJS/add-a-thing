007fdfff322bcb4cef7797faceaf038b3dd4b59c
Larionovv <larionovv@i.ua>
2019-05-07 18:19:20 +0300

    create delete component

--- src/components/TaskDetails/TaskDetailsModal.jsx
+++ src/components/TaskDetails/TaskDetailsModal.jsx
@@ -4,15 +4,31 @@ import {
 } from 'react-bootstrap';
 import TaskDetails from './TaskDetails';
 import SubTaskList from '../SubTaskList/SubTaskList';
+import DeleteTask from "./DeleteTask";
 
 export default class TaskDetailsModal extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showDelete: false,
+    };
+  }
+
   closeTaskDetails = () => {
     const { onClose: close } = this.props;
     close();
   };
 
+  closeDeleteModal = () => {
+    const { showDelete } = this.state;
+    this.setState({
+      showDelete: !showDelete,
+    });
+  };
+
   render() {
     const { taskRef, show: modalShow } = this.props;
+    const { showDelete } = this.state;
     this.modalShow = modalShow;
     return (
       <Modal
@@ -35,6 +51,19 @@ export default class TaskDetailsModal extends React.Component {
             <SubTaskList taskRef={taskRef} />
           </Container>
         </Modal.Body>
+        <Button
+          variant="outline-danger"
+          onClick={() => this.setState({ showDelete: !showDelete })}
+          className="delete-button"
+        >
+          {'Delete'}
+        </Button>
+        <DeleteTask
+          taskRef={taskRef}
+          showDelete={showDelete}
+          closeTaskDetails={this.closeTaskDetails}
+          closeDeleteModal={this.closeDeleteModal}
+        />
       </Modal>
     );
   }
