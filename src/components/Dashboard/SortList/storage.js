cbb4e0925231b0bf92fdae6ffbe0d0a266f3d609
Manzhula David <da411d@gmail.com>
2019-05-11 22:17:19 +0300

    Fix storage

--- src/components/Dashboard/SortList/storage.js
+++ src/components/Dashboard/SortList/storage.js
@@ -1,15 +1,16 @@
 const storage = {
   get: (key) => {
-    const storageKey = `SortList_Data_${(key || 'null').toString().toLowerCase().replace(/[^0-9a-z]/ig, '')}`;
-    let data = localStorage.getItem(storageKey) || '{}';
+    let data = localStorage.getItem('SortList_Data') || '{}';
     data = JSON.parse(data);
-    return data;
+    return data[key];
   },
 
   set: (key, value) => {
-    const storageKey = `SortList_Data_${(key || 'null').toString().toLowerCase().replace(/[^0-9a-z]/ig, '')}`;
-    const data = JSON.stringify(value, null, 2);
-    localStorage.setItem(storageKey, data);
+    let data = localStorage.getItem('SortList_Data') || '{}';
+    data = JSON.parse(data);
+    data[key] = value;
+    data = JSON.stringify(data);
+    localStorage.setItem('SortList_Data', data);
   },
 };
 
