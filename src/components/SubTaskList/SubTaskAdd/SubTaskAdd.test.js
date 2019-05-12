79d1fc0521652cd8766d71cdb0798f4066e20dcf
Vitaliy Tkachuk <vitfl@protonmail.com>
2019-05-12 15:35:04 +0300

    SubTaskAdd: modify old tests, add new tests

--- src/components/SubTaskList/SubTaskAdd/SubTaskAdd.test.js
+++ src/components/SubTaskList/SubTaskAdd/SubTaskAdd.test.js
@@ -8,15 +8,27 @@ describe('SubTaskAdd component', () => {
     expect(subTaskAddComponent).toMatchSnapshot();
   });
 
-  it('should always render button with ".add-subtask-button" class', () => {
+  it('should always render one button with ".add-subtask-button" class', () => {
     const subTaskAddComponent = shallow(<SubTaskAdd />);
     const button = subTaskAddComponent.find('.add-subtask-button');
-    expect(button.length).toBeGreaterThan(0);
+    expect(button.length).toBe(1);
   });
 
-  it('should always render text input with ".new-subtask-text" class', () => {
+  it('should always render one text input with ".new-subtask-text" class', () => {
     const subTaskAddComponent = shallow(<SubTaskAdd />);
     const input = subTaskAddComponent.find('.new-subtask-text');
-    expect(input.length).toBeGreaterThan(0);
+    expect(input.length).toBe(1);
+  });
+
+  it('if taskStatus is "Done", then button should be disabled', () => {
+    const subTaskAddComponent = shallow(<SubTaskAdd taskStatus="Done" />);
+    const button = subTaskAddComponent.find('.add-subtask-button');
+    expect(button.prop('disabled')).toBeTruthy();
+  });
+
+  it('if taskStatus is "Done", then text input should be disabled', () => {
+    const subTaskAddComponent = shallow(<SubTaskAdd taskStatus="Done" />);
+    const input = subTaskAddComponent.find('.new-subtask-text');
+    expect(input.prop('disabled')).toBeTruthy();
   });
 });
