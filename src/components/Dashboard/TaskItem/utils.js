1059a84414195650c4bbd8c9619d7f236059a568
Nazar Shniak <n.shniak@gmail.com>
2019-05-12 18:48:08 +0300

    Correct visual logic of DnD; fix styling errors and other minor bugs

--- src/components/Dashboard/TaskItem/utils.js
+++ src/components/Dashboard/TaskItem/utils.js
@@ -4,8 +4,9 @@ export const getTaskRef = (taskListRef, id) => {
 };
 
 export const getTaskStyleByPriority = (priority) => {
-  const baseStyle = 'task-item d-flex justify-content-between';
-  const finalStyle = priority ? `${baseStyle} ${priority}` : `${baseStyle}`;
+  const baseClass = 'task-item d-flex justify-content-between';
+  const priorityClass = `${priority}-task`;
+  const finalStyle = priority ? `${baseClass} ${priorityClass}` : `${baseClass}`;
   return finalStyle;
 };
 
@@ -13,3 +14,20 @@ export const getTaskStyleByStatus = (status) => {
   const style = status === 'Done' ? 'task-item-crossed' : '';
   return style;
 };
+
+export const dragStart = (event) => {
+  event.dataTransfer.setData('taskID', event.target.id);
+  event.target.classList.add('dragged');
+
+  const fakeTask = event.target.cloneNode(true);
+  fakeTask.classList.add('drag-avatar');
+  event.target.parentNode.parentNode.appendChild(fakeTask);
+
+  event.dataTransfer.setDragImage(fakeTask, 60, 25);
+}
+
+export const dragEnd = (event) => {
+  event.target.classList.remove('dragged');
+  const fakeTask = document.getElementsByClassName('drag-avatar');
+  while (fakeTask.length > 0) fakeTask[0].remove();
+}
