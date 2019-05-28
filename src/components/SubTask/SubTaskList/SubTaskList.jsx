dc81223618cedbd92e387a58f2090a64437c88ad
Vitaliy Tkachuk <vitfl@protonmail.com>
2019-05-29 02:19:01 +0300

    moved getVisibleSubTasks function to subTaskUtils.js

--- src/components/SubTask/SubTaskList/SubTaskList.jsx
+++ src/components/SubTask/SubTaskList/SubTaskList.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import SubTaskItem from '../SubTaskItem/SubTaskItem';
 import * as subTaskActions from '../../../actions/subTaskActions';
-import { subtaskFilterTypes } from '../subTaskFilterTypes';
+import { getVisibleSubTasks } from '../subTaskUtils';
 
 class SubTaskList extends Component {
   constructor(props) {
@@ -15,24 +15,11 @@ class SubTaskList extends Component {
     fetchInfoForSubTaskList(taskRef);
   }
 
-  getVisibleSubTasks = (subtaskList, currentFilter) => {
-    switch (currentFilter) {
-      case subtaskFilterTypes.SHOW_ALL:
-        return subtaskList;
-      case subtaskFilterTypes.SHOW_COMPLETED:
-        return subtaskList.filter(subtask => subtask.completed);
-      case subtaskFilterTypes.SHOW_ACTIVE:
-        return subtaskList.filter(subtask => !subtask.completed);
-      default:
-        throw new Error(`Unknown filter: ${currentFilter}`);
-    }
-  }
-
   render() {
     const {
       taskRef, taskStatus, subtaskList = {}, currentFilter,
     } = this.props;
-    const subTaskItems = this.getVisibleSubTasks(subtaskList, currentFilter).map(subtask => (
+    const subTaskItems = getVisibleSubTasks(subtaskList, currentFilter).map(subtask => (
       <SubTaskItem
         key={subtask.id}
         id={subtask.id}
--- src/components/SubTask/subTaskUtils.js
+++ src/components/SubTask/subTaskUtils.js
@@ -1,3 +1,5 @@
+import { subtaskFilterTypes } from './subTaskFilterTypes';
+
 export const getSubtaskListAsArray = (snapValue = {}) => {
   if (typeof snapValue !== 'object' && snapValue.constructor !== Object) return [];
   const subtaskList = [];
@@ -11,3 +13,16 @@ export const getSubtaskListAsArray = (snapValue = {}) => {
   });
   return subtaskList;
 };
+
+export const getVisibleSubTasks = (subtaskList, currentFilter) => {
+  switch (currentFilter) {
+    case subtaskFilterTypes.SHOW_ALL:
+      return subtaskList;
+    case subtaskFilterTypes.SHOW_COMPLETED:
+      return subtaskList.filter(subtask => subtask.completed);
+    case subtaskFilterTypes.SHOW_ACTIVE:
+      return subtaskList.filter(subtask => !subtask.completed);
+    default:
+      throw new Error(`Unknown filter: ${currentFilter}`);
+  }
+}
