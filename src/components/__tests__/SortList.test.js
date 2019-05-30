53170724d89ae8c9baea220416e22eaa6aecc00b
Manzhula David <da411d@gmail.com>
2019-05-30 15:36:25 +0300

    Move tests to tests folder

---
+++ src/components/__tests__/SortList.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow, render } from 'enzyme';
+import { SortListComponent } from '../Dashboard/SortList/SortList';
+
+describe('<SortList />', () => {
+  it('should render and match snapshot', () => {
+    const testActions = {
+      loadSort: jest.fn(),
+    };
+    const testAllSortData = {
+      test: {
+        direction: 'NONE',
+      },
+    };
+    const renderTest = render(
+      <SortListComponent
+        storageKey="test"
+        sortIconColor="#ffc000"
+        sortListActions={testActions}
+        allSortData={testAllSortData}
+      />,
+    );
+    expect(renderTest).toMatchSnapshot();
+  });
+
+  it('should render dropdown', () => {
+    const testActions = {
+      loadSort: jest.fn(),
+    };
+    const testAllSortData = {
+      test: {
+        direction: 'NONE',
+      },
+    };
+    const renderTest = shallow(
+      <SortListComponent
+        storageKey="test"
+        sortIconColor="#ffc000"
+        sortListActions={testActions}
+        allSortData={testAllSortData}
+      />,
+    );
+    const dropdown = renderTest.find('Dropdown');
+    expect(dropdown.length).toBe(1);
+  });
+});
