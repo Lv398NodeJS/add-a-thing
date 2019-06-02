c12d2c51cd233c5657caef50edc7b7270db20028
Vitaliy Tkachuk <vitfl@protonmail.com>
2019-06-02 03:59:07 +0300

    Cover SubtaskEdit component with Unit Tests

---
+++ client/src/components/__tests__/SubtaskEdit.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { SubtaskEditComponent } from '../Subtask/SubtaskItem/SubtaskEdit';
+
+describe('SubtaskEdit component', () => {
+  const subtaskActions = { editSubtaskText: jest.fn() };
+  it('should render correctly and match the snapshot', () => {
+    const subtaskEditComponent = shallow(
+      <SubtaskEditComponent subtaskActions={subtaskActions} />,
+    );
+    expect(subtaskEditComponent).toMatchSnapshot();
+  });
+
+
+  it('should contain FormControl input with .new-subtask-name class name', () => {
+    const subtaskEditComponent = shallow(
+      <SubtaskEditComponent
+        subtaskActions={subtaskActions}
+      />,
+    );
+    expect(subtaskEditComponent.find('FormControl').hasClass('new-subtask-name')).toBeTruthy();
+  });
+
+
+  it('should contain FormControl input with placeholder "Enter subtask name here..."', () => {
+    const subtaskEditComponent = shallow(
+      <SubtaskEditComponent
+        subtaskActions={subtaskActions}
+      />,
+    );
+    expect(subtaskEditComponent.find('FormControl').prop('placeholder')).toBe('Enter subtask name here...');
+  });
+
+  it('should contain Button with .subtask-save-button class name', () => {
+    const subtaskEditComponent = shallow(
+      <SubtaskEditComponent
+        subtaskActions={subtaskActions}
+      />,
+    );
+    expect(subtaskEditComponent.find('Button').hasClass('subtask-save-button')).toBeTruthy();
+  });
+
+  it('should contain div with .valid-feedback class name', () => {
+    const subtaskEditComponent = shallow(
+      <SubtaskEditComponent
+        subtaskActions={subtaskActions}
+      />,
+    );
+    expect(subtaskEditComponent.find('.valid-feedback').length).toBe(1);
+  });
+
+  it('should contain div with .invalid-feedback class name', () => {
+    const subtaskEditComponent = shallow(
+      <SubtaskEditComponent
+        subtaskActions={subtaskActions}
+      />,
+    );
+    expect(subtaskEditComponent.find('.invalid-feedback').length).toBe(1);
+  });
+});
