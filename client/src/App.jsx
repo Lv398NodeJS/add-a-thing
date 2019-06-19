a11bb7e5ab5b8464546b38c2fb2bef0381b5a5c4
Larionovv <larionovv@i.ua>
2019-06-19 17:54:00 +0300

    fixed small bug with token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,10 +15,11 @@ export class App extends Component {
     const loggedInData = {
       token: localStorage.getItem('token'),
     };
+    if(localStorage.getItem('token')) {
       loggedUser(loggedInData);
-    // if(localStorage.getItem('token')) {
-    //   loggedUser();
-    // }
+    } else {
+      localStorage.clear();
+    }
   }
 
   render() {
